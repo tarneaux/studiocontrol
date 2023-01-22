ba8f82b827af9c850dc871b813f4b1b602cffe3e
tarneaux <tarneo@tarneo.fr>
2023-01-22 10:55:15 +0100

    Initial commit
    Generated by create-expo-app 1.1.6.

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Button title="shoot a photo" onPress={shootPhoto}></Button>
+    </View>
+  );
+}
+
+function shootPhoto() {
+  console.log("shoot a photo");
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
